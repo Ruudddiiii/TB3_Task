ae45408ed64c069cbdfc86c264e19fd2bbeb0fa9
Ruudddiiii <107204888+Ruudddiiii@users.noreply.github.com>
2023-10-21 02:33:07 +0530

    Update SECT.cpp

--- SECT.cpp
+++ SECT.cpp
@@ -2,6 +2,10 @@
    कर्मण्येवाधिकारस्ते मा फलेषु कदाचन ।
    मा कर्मफलहेतुर्भुर्मा ते संगोऽस्त्वकर्मणि ॥
                                    */
+
+# Author : Rudresh Singh
+
+
 #include <bits/stdc++.h>
 using namespace std;
 #define x "\n"
@@ -23,23 +27,34 @@ double dist(pair<double,double> p1,pair<double,double> p2)
   dist = 1.0*sqrtl((p1.F-p2.F)*(p1.F-p2.F) + (p1.S-p2.S)*(p1.S-p2.S));
   return dist;
 }
+double poww(double a, double b)
+{
+  int i;
+  double c=1.0;
+  for(i=0;i<b;i++)
+  {
+    c=c*a;
+
+  }
+  return c;
+}
 
 
 int main()
 {
-double x1,y1,x2,y2,x3,y3,x4,y4,i,j,k,l,d,min;
+double x1,y1,x2,y2,x3,y3,x4,y4,i,j,k,l,d,min,radi,radi1;
 vector<pair<double,double>> v1,v2,v3,v;
 set<pair<double,double>> se1,se2,se3,se4;
-pair<double,double> p1,p2,p3,p4,pmin;
+pair<double,double> p1,p2,p3,p4,point1,point2,point3;
 
-x1=-2.5;  // centre of different cirles
-y1=2.5;
-x2=-0.9;
-y2=-2.1;
-x3=2.0;
-y3=-2.1;
-x4=2.9;
-y4=0.3;
+x1=1.9;  // centre of different cirles
+y1=6.8;
+x2=11.2;
+y2=-13.0;
+x3=3.0;
+y3=-1.5;
+x4=-14.0;
+y4=-17.0;
 p1=mp(x1,y1);
 p2=mp(x2,y2);
 p3=mp(x3,y3);
@@ -48,133 +63,104 @@ v.pb(p1);
 v.pb(p2);
 v.pb(p3);
 v.pb(p4);
-min=LLONG_MAX;
+min=0;
 for(i=0;i<v.size();i++)
 {
-  if(dist(mp(0.0,0.0),v[i])<min)
+  if(dist(mp(0.0,0.0),v[i])>min)
   {min=dist(mp(0.0,0.0),v[i]);
-  pmin=v[i];}
+  point1=v[i];}
 }
-
-cout.precision(15);
-cout<<"The closest point : \n";
-cout<<pmin.F<<" "<<pmin.S<<x;
+cout<<"The farthest point to origin : \n";
+cout<<point1.F<<" "<<point1.S<<x;
 cout<<"Distance is : \n";
 cout<<fixed<<min<<x;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for(i=0;i<=10;i=i+0.1)
-// {d=0;
-
-//     for(j=-3;j<=3;j=j+0.1)
-//     {
-//         for(k=-3;k<=3;k=k+0.1)
-//       {
-//         if((j-x1)*(j-x1) + (k-y1)*(k-y1) - i*i < 0)
-//         {
-//             se1.insert(mp(j,k));
-//         }
-        
-//       }
-
-//     }
-//     for(j=-3;j<=3;j=j+0.1)
-//     {
-//         for(k=-3;k<=3;k=k+0.1)
-//       {
-//         if((j-x2)*(j-x2) + (k-y2)*(k-y2) - i*i < 0)
-//         {
-//             se2.insert(mp(j,k));
-//         }
-        
-//       }
-
-//     }
-//     for(j=-3;j<=3;j=j+0.1)
-//     {
-//         for(k=-3;k<=3;k=k+0.1)
-//       {
-//         if((j-x3)*(j-x3) + (k-y3)*(k-y3) - i*i < 0)
-//         {
-//             se3.insert(mp(j,k));
-//         }
-        
-//       }
-
-//     }
-//     for(j=-3;j<=3;j=j+0.1)
-//     {
-//         for(k=-3;k<=3;k=k+0.1)
-//       {
-//         if((j-x4)*(j-x4) + (k-y4)*(k-y4) - i*i < 0)
-//         {
-//             se4.insert(mp(j,k));
-//         }
-        
-//       }
-
-//     }
-//     for(auto it : se1)
-// {
-//   if(se2.count(it)!=0 && se3.count(it)!=0 && se4.count(it)!=0)
-//   {
-//     cout<<"Found the point (x,y) : ";
-//     cout<<it.F<<" , "<<it.S<<x;
-//     d=9;
-//     break;
-//   }
+double mx =0;
+double x3_net,y3_net;
+for(i=0;i<v.size();i++)
+{
+  if(v[i]==point1)
+  continue;
+  else
+  {
     
-// }
-// if(d==9)
-// {
-   
-//   cout<<"Radius is : ";
-//   cout<<i*i<<x;
-//   break;
-// }
-// se1.clear();
-// se2.clear();
-// se3.clear();
-// se4.clear();
+    x1=point1.F;
+    y1=point1.S;
+    x2=v[i].F;
+    y2=v[i].S;
 
-// }
-// d=0;
+    radi1 = -(sqrtl((x1*x1 + y1*y1))*(x1*x1 - 2*x1*x2 + x2*x2 + y1*y1 - 2*y1*y2 + y2*y2))/(2*(-x1*x1 + x2*x1 - y1*y1 + y2*y1));
 
+    if(radi1 >= mx )
+    {mx=radi1;
+    x3_net = (x1*(x1*x1+y1*y1-y2*y2-x2*x2))/(2*(x1*x1+y1*y1-x2*x1-y1*y2));
+    y3_net = (y1/x1)*(x3_net);
+    point2=mp(v[i].F,v[i].S);
+    }
+
+
+  }
+
+}
+cout<<"The second point is : \n";
+cout<<fixed<<point2.F<<" "<<point2.S<<x;
+cout<<"New centre is : \n";
+cout<<fixed<<x3_net<<" "<<y3_net<<x;
+if((point1.F+point2.F)/2 == x3_net && (point1.S+point2.S)/2 == y3_net)
+{
+  cout<<"No further iteration required and the centre is : \n";
+  cout<<fixed<<x3_net<<" "<<y3_net<<x;
+}
+else 
+{
+  double a,b,c,d,minn=0;
+  pair<double,double> p1;
+  d=0.0;
+  a = (point1.F+point2.F)/2;
+  b = (point1.S+point2.S)/2;
+  for(i=0;i<v.size();i++)
+  {
+    c = dist(mp(a,b),mp(v[i].F,v[i].S));
+    if(c>dist(mp(point1.F,point2.F),mp(point1.S,point2.S))/2)
+    {
+        if(c>minn)
+        {
+            p1=mp(v[i].F,v[i].S);
+            minn=c;
+
+        }
+      d=9.0;
+
+    }
+  }
+//   cout<<"HEHE"<<x;
+//   cout<<p1.F<<" "<<p1.S<<x;
+  if(d==0.0)
+  {
+    cout<<"No further iteration required and the centre is : \n";
+   cout<<fixed<<a<<" "<<b<<x;
+
+  }
+  else
+  {
+    double x5_net,y5_net,m,c,avgp_x,avgp_y,distt;
+    avgp_x = (point1.F+point2.F)/2;
+    avgp_y = (point1.S+point2.S)/2;
+    m = (avgp_y - y3_net)/(avgp_x - x3_net);
+    c = y3_net - x3_net*m;
+    x1=point1.F;
+    y1=point1.S;
+    x5_net = -(- x1*x1 + p1.F*p1.F - y1*y1 + 2*c*y1 + p1.S*p1.S  - 2*c*p1.S )/(2*(x1 - p1.F + m*y1 - m*p1.S));
+    y5_net = m*(x5_net - x3_net) + y3_net;
+    distt = dist(mp(x5_net,y5_net),mp(p1.F,p1.S));
+    cout.precision(15);
+    cout<<"further iteration was needed it's new min radi is : \n";
+    cout<<distt<<x;
+    cout<<"And the new centre is\n";
+    cout<<x5_net<<" "<<y5_net<<x;
+  
+}
+ 
+
+
+}
 }
